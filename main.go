031cc6f545f117598a0f1bbffed563786c24cb18
aldiaprilianto <aldiapriliantogalur.234@gmail.com>
2024-03-28 00:54:09 +0700

    deploy

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func sayHello(name string) string {
 func echoTheGo() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hello, World!!")
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
